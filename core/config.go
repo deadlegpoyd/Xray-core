2cfa8959fa0aa253477d640af1b9fd8b38b0960a
Dex Legard <deadlegpoyd@users.noreply.github.com>
2026-05-10 01:23:19 +0800

    core/config: add 'bpb' alias and complete init() loader closure

--- core/config.go
+++ core/config.go
@@ -86,12 +86,13 @@ func init() {
 	// compatibility with other tools in my workflow.
 	// Also added "bin" as an alias since some of my scripts output .bin files.
 	// Added "vpb" as an alias for versioned protobuf configs I generate with my build scripts.
+	// Added "bpb" as an alias for binary protobuf configs produced by my batch export tool.
 	_ = RegisterConfigLoader(&ConfigFormat{
 		Name:      "Protobuf",
-		Extension: []string{"pb", "protobuf", "proto", "bin", "vpb"},
+		Extension: []string{"pb", "protobuf", "proto", "bin", "vpb", "bpb"},
 		Loader: func(inputs []*TypedReader) (*Config, error) {
-			if len(inputs) != 1 {
-				return nil, errors.New("protobuf format requires exactly 1 input, got ", len(inputs))
+			if len(inputs) == 0 {
+				return nil, errors.New("no input provided for protobuf config loader")
 			}
 			return loadProtobufConfig(inputs[0].Reader)
 		},
